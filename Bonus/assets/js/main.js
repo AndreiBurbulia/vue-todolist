a0c1cbbdde3f226f2f0c172411027d0a75e1003e
AndreiBurbulia <Andreiburbulia@hotmail.it>
2021-05-19 18:15:56 +0200

    Pulizia Codice

--- Bonus/assets/js/main.js
+++ Bonus/assets/js/main.js
@@ -1,13 +1,3 @@
-/*
-Bonus (opzionale)
-Quando una task é stata completa allora vuole che venga inserita in un'altra colonna tipo "tasks completate"
-ma vuole anche poter indicare che la task é stata completata (con un icona cliccabile)
-se una task é stata completa allora vuole che venga inserita in un'altra colonna tipo "tasks completate"
-se una task é stata marcata come completa per sbaglio allora vuole poterla rimettere nella todo list (cliccando su un altra icona)
-ah non é finita, dice che quando cancella una task non vuole che questa venga subito rimossa, ma vuole che resti visibile ma venga spostata in una colonna tipo "cestino"
-si, l'utente é un rompi scatole, dice infine che vuole poter rimuovere tutte le tasks nel cestino cliccando su un pulsante tipo "svuota cestino"
-Il nostro utente per ora sembra non avere altre richieste ... ma chissá se dopo gli viene in mente che vuole anche poter rimettere una task cancellata nella lista di tasks da fare, magari l'ha cancellata per sbaglio.
-*/
 
 const app = new Vue({
     el: "#app",
@@ -54,15 +44,6 @@ const app = new Vue({
         deleteAllTask() {
             this.taskDelete = [];
         },
-    },
-
-    mounted() {
-        document.getElementById("inputTask").addEventListener("keyup", (e) => {
-            let tasto = e.key;
-            if (tasto === "Enter") {
-                this.addTask();
-            }
-        })
     },
 
 })
